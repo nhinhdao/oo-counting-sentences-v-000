252e1586952f9e2a340c721c505364d9d8a937f2
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-24 03:50:32 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+      self[-1] == '.'
   end
 
   def question?
-
+    self[-1] == '?'
   end
 
   def exclamation?
-
+    self[-1] == '!'
   end
 
   def count_sentences
-
+    count = 0
+    # self.each_char { |chr|  count += 1 if chr == "." || chr == "!" || chr == "?"}
+    # this code doesn't work for: "Hi!! I am Nhinh...."
+    for i in 0..self.length do
+      count += 1 if ((self[i] == "." || self[i] == "!" || self[i] == "?") && self[i+1] != self[i])
+    end
+    count
   end
 end
